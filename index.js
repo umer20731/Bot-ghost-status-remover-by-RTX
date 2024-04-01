98a22afebb5a9665232f8352715d01977936945e
umer20731 <36644222+umer20731@users.noreply.github.com>
2024-04-01 17:41:12 +0500

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Watching Over Kermit Productions.👀","Watching https://discord.gg/eeQRnTqFyr"];
 
 
 let currentIndex = 0;
